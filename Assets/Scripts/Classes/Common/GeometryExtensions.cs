3bb6d20b21599c3b6a93d109b435aac7c41181c7
Carambula <carambuletka@gmail.com>
2021-12-25 19:44:11 +0200

    add FindClosest to GeometryExtensions

--- Assets/Scripts/Classes/Common/GeometryExtensions.cs
+++ Assets/Scripts/Classes/Common/GeometryExtensions.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace BattleCity.Common
 {
@@ -11,6 +13,20 @@ namespace BattleCity.Common
         public static Vector3 ReProjectFromYZ(this in Vector2 v, float newX = 0.0f) => new Vector3(newX, v.x, v.y);
         public static Vector3 ReProjectFromXZ(this in Vector2 v, float newY = 0.0f) => new Vector3(v.x, newY, v.y);
         public static Vector3 ReProjectFromXY(this in Vector2 v, float newZ = 0.0f) => new Vector3(v.x, v.y, newZ);
+
+        public static Vector2 GetClosest(this in Vector2 v, in IEnumerable<Vector2> possibleValues) {
+            Vector2 result = Vector2.zero;
+            var minAngle = float.MaxValue;
+            foreach (var vect in possibleValues) {
+                var angle = Vector2.Angle(v, vect);
+                if (angle < minAngle) {
+                    result = vect;
+                    minAngle = angle;
+                }
+            }
+
+            return result;
+        }
 
         public static void Deconstruct(this Vector2Int v, out int x, out int y)
         {
