16a5747b8747109a70072ac585b959b4432bfd76
Carambula <carambuletka@gmail.com>
2021-12-25 19:48:05 +0200

    fix handling null after FindShortestPath

--- Assets/Scripts/Classes/AI/Pathfinding/FieldPathfinderHelper.cs
+++ Assets/Scripts/Classes/AI/Pathfinding/FieldPathfinderHelper.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿#nullable enable
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
@@ -16,15 +18,16 @@ namespace BattleCity.AI
             _pathfinder = pathfinder;
         }
         
-        public Vector3[] FindShortestPath(Vector3 start, Vector3 goal)
+        public Vector3[]? FindShortestPath(Vector3 start, Vector3 goal)
         {
-            Vector2Int[] fieldPath = _pathfinder.FindShortestPath(
+            Vector2Int[]? fieldPath = _pathfinder.FindShortestPath(
                 Converter.Convert(start), 
                 Converter.Convert(goal), 
                 _fieldContainerManager.Field
             );
 
-            return fieldPath.Select(Converter.Convert).ToArray();
+
+            return fieldPath?.Select(Converter.Convert).ToArray();
         }
         public int GetShortestPathLength(Vector2 start, Vector2 goal)
         {
