deb94bf83930c416494b6190747ec0c6946c7849
Carambula <carambuletka@gmail.com>
2021-12-25 19:43:35 +0200

    add FloatExtensions

---
+++ Assets/Scripts/Classes/Common/FloatExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BattleCity.Common {
+    public static class FloatExtensions {
+        public static bool Equals(this float num1, float num2, float accuracy = Consts.EPSILON) {
+            return Math.Abs(num1 - num2) < accuracy;
+        }
+
+        public static bool Equals(this float num1, int num2, float accuracy = Consts.EPSILON) {
+            return Math.Abs(num1 - num2) < accuracy;
+        }
+    }
+}
