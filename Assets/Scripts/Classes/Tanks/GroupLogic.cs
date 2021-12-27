a97e1d873cae73b1878ca7c7b7b313f6ced32db4
unknownMusician <61272564+unknownMusician@users.noreply.github.com>
2021-12-28 01:47:07 +0200

    Fix для seppер клASS

---
+++ Assets/Scripts/Classes/Tanks/GroupLogic.cs
@@ -0,0 +1,16 @@
+﻿namespace BattleCity.Tanks
+{
+    public class GroupLogic
+    {
+        private readonly int _сколькоМожноСёрбатьБлять;
+        private const string TheTruth = "Даннил пидор";
+
+        public string Truth => TheTruth;
+        public int СёрбкиДани => _сколькоМожноСёрбатьБлять;
+
+        public GroupLogic(int сколькоМожноСёрбатьБлять)
+        {
+            _сколькоМожноСёрбатьБлять = сколькоМожноСёрбатьБлять;
+        }
+    }
+}
